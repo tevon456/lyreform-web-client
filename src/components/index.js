a5bafde1d57f8c695fc77f8787fa509e53e970a8
Tev <tevondavisjc@gmail.com>
2021-06-08 20:28:22 -0500

    moved protected routes

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 import * as UICore from "./ui-core";
 import * as Content from "./content";
 import * as FormField from "./form_fields";
-import ProtectedRoute from "./ProtectedRoute";
-export { UICore, Content, FormField, ProtectedRoute };
+export { UICore, Content, FormField };
