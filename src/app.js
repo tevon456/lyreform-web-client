cc6fe5637de755412c1a3986f1c574249402b275
Tev <tevondavisjc@gmail.com>
2021-06-08 20:28:07 -0500

    moved routes

--- src/app.js
+++ src/app.js
@@ -1,23 +1,11 @@
 import "./styles/app.css";
 import React, { useContext } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
-import { Content, ProtectedRoute, UICore } from "./components";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Content, UICore } from "./components";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SessionContext } from "./context/Session";
-import Home from "./pages/home";
-import NotFound from "./pages/not_found";
-import Signup from "./pages/signup";
-import VerifyAccount from "./pages/verify_account";
-import ResendConfirmation from "./pages/resend_confirmation";
-import Login from "./pages/login";
-import ForgotPassword from "./pages/forgot_password";
-import NewPassword from "./pages/new_password";
+import ApplicationRoutes from "./route/ApplicationRoutes";
 
 export default function App() {
   const { hasValidSession, user, endSession } = useContext(SessionContext);
@@ -59,33 +47,7 @@ export default function App() {
   return (
     <Router>
       <Content.Navbar links={links} />
-      <Switch>
-        <ProtectedRoute exact path="/" component={Home} />
-        <Route exact path="/forgot-password" component={ForgotPassword} />
-        <Route path="/new-password/:token" component={NewPassword} />
-        <Route path="/verify-account/:token" component={VerifyAccount} />
-
-        {hasValidSession() ? (
-          <Route exact path="/login" render={() => <Redirect to="/" />} />
-        ) : (
-          <Route exact path="/login" component={Login} />
-        )}
-        {hasValidSession() ? (
-          <Route exact path="/signup" render={() => <Redirect to="/" />} />
-        ) : (
-          <Route exact path="/signup" component={Signup} />
-        )}
-        {hasValidSession() ? (
-          <Route
-            exact
-            path="/resend-confirmation"
-            render={() => <Redirect to="/" />}
-          />
-        ) : (
-          <Route path="/resend-confirmation" component={ResendConfirmation} />
-        )}
-        <Route component={NotFound} />
-      </Switch>
+      <ApplicationRoutes />
       <ToastContainer hideProgressBar={true} />
     </Router>
   );
