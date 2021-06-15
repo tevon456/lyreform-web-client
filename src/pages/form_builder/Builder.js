21b6ffe99be261483d6e411cdebed0ca98f76b0d
Tev <tevondavisjc@gmail.com>
2021-06-15 01:51:10 -0500

    modified builder

--- src/pages/form_builder/Builder.js
+++ src/pages/form_builder/Builder.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { UICore } from "../../components";
 import { useNavbar } from "../../hooks";
-import Checkbox from "./components/Checkbox";
-import Input from "./components/Input";
-import Select from "./components/Select";
+import Panel from "./components/Panel";
+import CanvasList from "./components/CanvasList";
 import Toolbar from "./components/Toolbar";
 
 export function Builder() {
@@ -19,41 +18,8 @@ export function Builder() {
       <Toolbar />
       <UICore.Box height="100vh" pd="0px">
         <UICore.Flex align="center">
-          <UICore.Box
-            height="100vh"
-            width="160px"
-            minWidth="130px"
-            border="1px solid var(--neutral-300)"
-            bg="#FFF"
-            pd="8px"
-            pt="60px"
-            style={{ flexGrow: 1 }}
-          >
-            <Input label="Label" mb="16px" width="140px" />
-            <Input label="Placeholder" mb="16px" width="140px" />
-            <Input type="number" label="Max length" mb="16px" width="140px" />
-            <Select
-              label="Required"
-              mb="16px"
-              width="150px"
-              options={["yes", "no"]}
-            />
-            <Checkbox baseColor="var(--primary)" label="Required" />
-
-            <UICore.Line
-              style={{ display: "block" }}
-              variant="h"
-              thickness="1px"
-              length="94%"
-              color="var(--neutral-350)"
-            />
-          </UICore.Box>
-          <UICore.Box
-            height="100vh"
-            width="100px"
-            pd="0px"
-            style={{ flexGrow: 8 }}
-          />
+          <Panel />
+          <CanvasList />
         </UICore.Flex>
       </UICore.Box>
     </UICore.Box>
