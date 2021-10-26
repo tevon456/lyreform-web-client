614a72ce10420e626b756b4681c7ab5e657ea850
Tev <tevondavisjc@gmail.com>
2021-10-26 11:35:52 -0500

    remove console log

--- src/pages/form_builder/form/GeneralForm.js
+++ src/pages/form_builder/form/GeneralForm.js
@@ -19,7 +19,6 @@ export default function GeneralForm({ form, id, triggerRender }) {
 
   useEffect(() => {
     setField(form.getFieldById(id)?.field[0]);
-    console.log(field.label);
     // eslint-disable-next-line
   }, [id, field]);
 
