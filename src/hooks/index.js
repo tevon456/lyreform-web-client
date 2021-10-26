f6be0832f26264227a22fa49b1ce3e619113fc5e
Tev <tevondavisjc@gmail.com>
2021-10-26 11:35:13 -0500

    new keypress hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,6 +2,7 @@ import useBackground from "./useBackground";
 import useRestResponse from "./useRestResponse";
 import { useWindowSize } from "./useWindowSize";
 import useOnClickOutside from "./useOnClickOutside";
+import useKeyPress from "./useKeyPress";
 import useNavbar from "./useNavbar";
 // import useLockBodyScroll from "./useLockBodyScroll";
 // import useInput from "./useInput";
@@ -13,6 +14,7 @@ export {
   useOnClickOutside,
   useBackground,
   useRestResponse,
+  useKeyPress,
   //   useDialog,
   //   useLockBodyScroll,
   //   useInput,
