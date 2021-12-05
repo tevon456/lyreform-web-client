523d15b810f2d225d9c0ac8dc602c1e08b543a88
Tev <tevondavisjc@gmail.com>
2021-12-04 22:56:00 -0500

    add Icons

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import * as UICore from "./ui-core";
 import * as Content from "./content";
 import * as FormField from "./form_fields";
-export { UICore, Content, FormField };
+import * as Icons from "@heroicons/react/outline";
+export { UICore, Content, FormField, Icons };
