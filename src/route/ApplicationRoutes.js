bdc315c783def6b00e12822ab2896cf2093742c2
Tev <tevondavisjc@gmail.com>
2021-12-29 17:11:56 -0500

    dashboard routes

--- src/route/ApplicationRoutes.js
+++ src/route/ApplicationRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
-import Home from "../pages/home";
+import Dashboard from "../pages/dashboard";
 import NotFound from "../pages/not_found";
 import Signup from "../pages/signup";
 import VerifyAccount from "../pages/verify_account";
@@ -9,17 +9,20 @@ import ResendConfirmation from "../pages/resend_confirmation";
 import Login from "../pages/login";
 import ForgotPassword from "../pages/forgot_password";
 import NewPassword from "../pages/new_password";
+import Form from "../pages/form";
 import { Builder } from "../pages/form_builder/Builder";
 import { SessionRoute } from ".";
 
 export default function ApplicationRoutes() {
   return (
     <Switch>
-      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute path="/dashboard" component={Dashboard} />
       <ProtectedRoute exact path="/builder" component={Builder} />
+      <ProtectedRoute path="/form/:id" component={Form} />
       <Route exact path="/forgot-password" component={ForgotPassword} />
       <Route path="/new-password/:token" component={NewPassword} />
       <Route path="/verify-account/:token" component={VerifyAccount} />
+      <SessionRoute exact path="/" redirectTo="/dashboard" component={Login} />
       <SessionRoute exact path="/login" redirectTo="/" component={Login} />
       <SessionRoute exact path="/signup" redirectTo="/" component={Signup} />
       <SessionRoute
