1857b4c6cc555ababfeeb290ce9881097f9a872b
Tev <tevondavisjc@gmail.com>
2022-01-11 15:18:15 -0500

    create render responses for use in panel map

---
+++ src/pages/dashboard/components/RenderResponse.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { UICore } from "../../../components";
+
+export default function RenderResponse({ type, responseKey, data }) {
+  const formatter = new Intl.ListFormat("en-US", {
+    style: "long",
+    type: "conjunction",
+  });
+  switch (type) {
+    case "CHECKBOX_GROUP":
+      return (
+        <>
+          <UICore.Flex>
+            <ResponseLabel>{data[responseKey].label}</ResponseLabel>
+          </UICore.Flex>
+          <UICore.Text mt="2px">
+            {formatter.format(data[responseKey].value)}
+          </UICore.Text>
+        </>
+      );
+    case "EMAIL":
+      return (
+        <>
+          <ResponseLabel>{data[responseKey].label}</ResponseLabel>
+          <a href={`mailto:${data[responseKey].value}`}>
+            <UICore.Text color="blue" mt="2px" weight="300">
+              {data[responseKey].value}
+            </UICore.Text>
+          </a>
+        </>
+      );
+    case "EMPTY":
+      return (
+        <>
+          <UICore.Flex>
+            <UICore.Badge
+              background="#bcdfff"
+              color="var(--accent-color)"
+              align="left"
+              data-balloon-length="large"
+              aria-label="As you update your form with new fields old responses are filled in to reflect the changes."
+              data-balloon-pos="right"
+            >
+              {data[responseKey].label}
+            </UICore.Badge>
+          </UICore.Flex>
+          <UICore.Text mt="2px" weight="300">
+            {data[responseKey].value}
+          </UICore.Text>{" "}
+        </>
+      );
+    default:
+      return (
+        <>
+          <ResponseLabel>{data[responseKey].label}</ResponseLabel>
+          <UICore.Text mt="2px" weight="300">
+            {data[responseKey].value}
+          </UICore.Text>{" "}
+        </>
+      );
+  }
+}
+
+const ResponseLabel = ({ children }) => (
+  <UICore.Text size="rg" mb="4px" weight="500" align="left">
+    {children}
+  </UICore.Text>
+);
