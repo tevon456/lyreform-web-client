562bdc9e898a550ca809ed5530bf2cd9b50049dd
Tev <tevondavisjc@gmail.com>
2022-01-11 15:17:38 -0500

    export inbox util

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,6 @@ import { Api } from "./api";
 import { Auth } from "./auth";
 import Notification from "./notification";
 import Lyreform from "./lyreform";
+import { InboxUtil } from "./inbox";
 
-export { Lyreform, Notification, Api, Auth };
+export { Lyreform, Notification, Api, Auth, InboxUtil };
