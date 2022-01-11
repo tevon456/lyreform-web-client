5d26ac463f5085b7d6d07c7c347ed02b58dd4df3
Tev <tevondavisjc@gmail.com>
2022-01-11 15:15:29 -0500

    create Inbox

--- src/pages/dashboard/Inbox.js
+++ src/pages/dashboard/Inbox.js
@@ -1,19 +1,88 @@
-import React from "react";
-import { UICore } from "../../components";
+import React, { useEffect, useState } from "react";
+import { Content, Icons, UICore } from "../../components";
 import "styled-components/macro";
+import { Api, InboxUtil, Lyreform } from "../../utils";
+import { useRestResponse } from "../../hooks";
+import { PanelMap } from "./components";
 
 export default function Inbox() {
+  const {
+    loading: formListLoading,
+    setLoading: setFormListLoading,
+    data: formList,
+    setData: setFormList,
+  } = useRestResponse([]);
+  const { data: responses, setData: setResponses } = useRestResponse([]);
+  const [currentForm, setCurrentForm] = useState();
+  const [activeId, setActiveId] = useState();
+
+  useEffect(() => {
+    Api.getAllForms()
+      .then((res) => {
+        setFormList(res.data.results);
+        setCurrentForm(res.data.results[0]);
+        setFormListLoading(false);
+      })
+      .catch((err) => {
+        setFormListLoading(false);
+      });
+    // eslint-disable-next-line
+  }, []);
+
+  if (formListLoading) {
+    return (
+      <UICore.Flex align="center" justify="center" style={{ height: "80vh" }}>
+        <UICore.Loader />
+      </UICore.Flex>
+    );
+  }
+
   return (
     <UICore.Box pd="0px" mt="52px" width="calc(100vw - 220px)">
       <UICore.Flex>
-        <SidePanel />
-        <MainPanel />
+        <SidePanel
+          setResponses={setResponses}
+          responses={responses}
+          formList={formList}
+          currentForm={currentForm}
+          setCurrentForm={setCurrentForm}
+          setActiveId={setActiveId}
+          activeId={activeId}
+        />
+        <MainPanel activeId={activeId} responses={responses} />
       </UICore.Flex>
     </UICore.Box>
   );
 }
 
-function SidePanel() {
+function SidePanel({
+  activeId,
+  responses = [],
+  formList = [],
+  currentForm = {},
+  setCurrentForm = () => {},
+  setResponses = () => {},
+  setActiveId = () => {},
+}) {
+  useEffect(() => {
+    Api.getFormSubmissions(currentForm?.uuid)
+      .then((res) => {
+        if (res?.data?.results.length > 0) {
+          setResponses(
+            InboxUtil.MissingKeys(res?.data?.results, {
+              label: "Empty",
+              value: "<no response>",
+              fieldType: "EMPTY",
+            }).reverse()
+          );
+        } else {
+          setResponses([]);
+        }
+      })
+      .catch((err) => {});
+    // eslint-disable-next-line
+  }, [currentForm]);
+
   return (
     <UICore.Box
       pd="0px"
@@ -31,7 +100,35 @@ function SidePanel() {
           mg="0px"
         >
           <UICore.Flex align="center">
-            <UICore.Button fullWidth>New Form</UICore.Button>
+            <Content.DropDown
+              width="180px"
+              items={formList.map((form) => {
+                return {
+                  type: "action",
+                  text: form?.name,
+                  onClick: () => {
+                    setCurrentForm(form);
+                    setActiveId(null);
+                  },
+                };
+              })}
+              x="0px"
+              y="12px"
+            >
+              <UICore.Button fullWidth title="switch form">
+                <Icons.SwitchVerticalIcon width="19px" height="19px" />
+              </UICore.Button>
+            </Content.DropDown>
+            <UICore.Space amount={2} />
+            <div
+              aria-label={currentForm?.name}
+              data-balloon-pos="down"
+              style={{ maxWidth: "150px" }}
+            >
+              <UICore.Text mt="0px" mb="0px" className=" truncate">
+                {currentForm?.name || "No form selected"}
+              </UICore.Text>
+            </div>
           </UICore.Flex>
         </UICore.Box>
         <UICore.Box
@@ -39,14 +136,68 @@ function SidePanel() {
             flex-grow: 1;
           `}
         >
-          sl
+          {responses.map((response) => (
+            <div
+              key={response.id}
+              css={`
+                margin-bottom: 4px;
+                border-radius: 4px;
+                &:hover {
+                  box-shadow: 0 0 0px 1px var(--primary);
+                }
+              `}
+            >
+              <UICore.Box
+                bg={activeId === response.id ? "#0066f5" : "var(--neutral-100)"}
+                color={activeId === response.id ? "#fff" : "var(--text-dark)"}
+                onClick={() => setActiveId(response?.id)}
+                radius="4px"
+                pd="4px"
+                css={`
+                  cursor: pointer;
+                  font-family: var(--font-secondary), var(--font-fallback);
+                `}
+              >
+                <UICore.Text mb="2px" mt="6px" weight="600" color="inherit">
+                  {response.data[Object.keys(response.data)[0]].value}
+                </UICore.Text>
+                <UICore.Text
+                  size="sm"
+                  mt="0px"
+                  mb="6px"
+                  color="inherit"
+                  weight="300"
+                >
+                  {new Date(response.createdAt).toLocaleString()}
+                </UICore.Text>
+              </UICore.Box>
+              <div className="hl" />
+            </div>
+          ))}
         </UICore.Box>
       </UICore.Flex>
     </UICore.Box>
   );
 }
 
-function MainPanel() {
+function MainPanel({ activeId, responses }) {
+  const [activeDetails, setActiveDetails] = useState();
+  let [responseKeys, setResponseKeys] = useState();
+
+  let utility = new Lyreform().util;
+
+  useEffect(() => {
+    setActiveDetails(utility.selectField(responses, "id", activeId)[0]);
+  }, [activeId]);
+
+  useEffect(() => {
+    if (activeDetails?.data) {
+      setResponseKeys(Object.keys(activeDetails?.data));
+    } else {
+      setResponseKeys([]);
+    }
+  }, [activeDetails]);
+
   return (
     <UICore.Box
       pd="0px"
@@ -65,7 +216,36 @@ function MainPanel() {
             <UICore.Button>New Form</UICore.Button>
           </UICore.Flex>
         </UICore.Box>
-        <UICore.Box>mp</UICore.Box>
+        <UICore.Box
+          css={`
+            height: calc(100vh - calc(64px * 2));
+            overflow-y: auto;
+
+            &::-webkit-scrollbar {
+              width: 12px;
+            }
+
+            &::-webkit-scrollbar-track {
+              background: #d9d9d9;
+            }
+
+            &::-webkit-scrollbar-thumb {
+              background-color: #a7a7a7;
+              border-radius: 20px;
+              border: 2px solid #d9d9d9;
+            }
+
+            &::-webkit-scrollbar-thumb:hover {
+              background-color: grey;
+            }
+          `}
+        >
+          <PanelMap
+            responseKeys={responseKeys}
+            data={activeDetails?.data}
+            details={activeDetails}
+          />
+        </UICore.Box>
       </UICore.Flex>
     </UICore.Box>
   );
