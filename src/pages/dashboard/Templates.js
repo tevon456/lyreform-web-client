fe9bbe761295df1dba1d1511f7f77ed994b1e2e1
Tev <tevondavisjc@gmail.com>
2022-01-27 12:55:24 -0500

    used subpage

--- src/pages/dashboard/Templates.js
+++ src/pages/dashboard/Templates.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Content, UICore } from "../../components";
 import "styled-components/macro";
+import { SubPage } from "./components";
 
 export default function Templates() {
   return (
-    <UICore.Page>
-      <UICore.Text weight="500" size="lg">
-        Templates
-      </UICore.Text>
+    <SubPage>
+      <UICore.Flex>
+        <UICore.Text weight="500" size="lg">
+          Templates
+        </UICore.Text>
+      </UICore.Flex>
+
       <Content.Card>
         <UICore.Text>Templates are coming soon</UICore.Text>
       </Content.Card>
-    </UICore.Page>
+    </SubPage>
   );
 }
