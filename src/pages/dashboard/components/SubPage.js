6b90814cd7c67d906fc927059c0ebbb68f8b0c61
Tev <tevondavisjc@gmail.com>
2022-02-20 16:10:31 -0500

    new subpage

--- src/pages/dashboard/components/SubPage.js
+++ src/pages/dashboard/components/SubPage.js
@@ -1,79 +1,61 @@
-import React from "react";
-import styled from "styled-components";
+import { UICore, Icons } from "../../../components";
+import { breakpoints } from "../../../utils";
 
-const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "425px",
-  tabletS: "475px",
-  tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
-};
+import "styled-components/macro";
+import { useHistory } from "react-router-dom";
 
-const device = {
-  mobileS: `(max-width: ${size.mobileS})`,
-  mobileM: `(max-width: ${size.mobileM})`,
-  mobileL: `(max-width: ${size.mobileL})`,
-  tabletS: `(max-width: ${size.tabletS})`,
-  tablet: `(max-width: ${size.tablet})`,
-  laptop: `(max-width: ${size.laptop})`,
-  laptopL: `(max-width: ${size.laptopL})`,
-  desktop: `(max-width: ${size.desktop})`,
-  desktopL: `(max-width: ${size.desktop})`,
-};
-
-const Main = styled.main`
-  padding-bottom: var(--space-md);
-  margin-left: 16px;
-
-  @media ${device.desktopL} {
-    padding-top: var(--space-xxl);
-    padding-bottom: var(--space-xxl);
-    padding-left: var(--space-xxl);
-    padding-right: var(--space-xxl);
-  }
-
-  @media ${device.desktop} {
-    padding-top: var(--space-xxl);
-    padding-bottom: var(--space-xxl);
-    padding-left: var(--space-xxl);
-    padding-right: var(--space-xxl);
-  }
-
-  @media ${device.laptop} {
-    padding-top: var(--space-xxl);
-    padding-bottom: var(--space-xxl);
-    padding-left: var(--space-md);
-    padding-right: var(--space-md);
-  }
-
-  @media ${device.tablet} {
-    padding-top: var(--space-xl);
-    padding-bottom: var(--space-xl);
-    padding-left: var(--space-md);
-    padding-right: var(--space-md);
-  }
-
-  @media ${device.mobileL} {
-    padding-top: var(--space-xl);
-    padding-bottom: var(--space-xl);
-    padding-left: var(--space-sm);
-    padding-right: var(--space-sm);
-  }
-`;
-
-/**
- * Used as parent for Page Components.
- * @prop {string} className: css class
- */
-function SubPage({ children, className, background, ...rest }) {
+export default function SubPage({ title, actions, children }) {
+  const history = useHistory();
   return (
-    <Main id="page" className={className} {...rest}>
-      {children}
-    </Main>
+    <main
+      css={`
+        padding: 2rem 12rem;
+        margin-bottom: 6rem;
+
+        @media screen and (max-width: 1260px) {
+          padding: 2rem 8rem;
+        }
+        @media screen and (max-width: 1060px) {
+          padding: 2rem 6rem;
+        }
+        @media screen and (max-width: 970px) {
+          padding: 2rem 4rem;
+        }
+        @media screen and (max-width: ${breakpoints.phablet}) {
+          padding: 2rem;
+        }
+        @media screen and (max-width: ${breakpoints.mobile}) {
+          padding: 1rem;
+        }
+      `}
+    >
+      <div>
+        <UICore.Flex
+          justify="space-between"
+          align="center"
+          className="margin-bottom--md"
+        >
+          <div>
+            <UICore.Flex align="center">
+              <div>
+                <UICore.Button
+                  kind="secondary"
+                  title="Back"
+                  onClick={() => history.goBack()}
+                >
+                  <Icons.ArrowLeft width="16px" height="16px" />
+                </UICore.Button>
+              </div>
+              <UICore.Space amount={2} />
+              <UICore.Text as="h1" size="lg" weight="bold" mt="0px" mb="0px">
+                {title}
+              </UICore.Text>
+            </UICore.Flex>
+          </div>
+          <div>{actions}</div>
+        </UICore.Flex>
+      </div>
+      <section>{children}</section>
+    </main>
   );
 }
-
-export default SubPage;
