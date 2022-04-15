fedc44088b27384fc9031a0546435a85ebee30ea
Tev <tevondavisjc@gmail.com>
2022-04-15 15:29:48 -0500

    new user context

---
+++ src/context/user.context.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect } from "react";
+import { Api } from "../utils";
+import { useAPI } from "../hooks";
+import { SessionContext } from "./session.context";
+
+const UserContext = React.createContext();
+function UserProvider({ children }) {
+  const {
+    user: { uuid = "" },
+  } = useContext(SessionContext);
+
+  const {
+    data: { data: user },
+    loading,
+    error,
+    refetch,
+  } = useAPI(() => Api.getUser(uuid), { name: "", email: "", uuid: "" });
+
+  return (
+    <UserContext.Provider value={{ user, loading, error, refetch }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export { UserContext, UserProvider };
