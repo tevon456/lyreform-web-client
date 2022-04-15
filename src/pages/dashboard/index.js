82235c810b5567c300f783e7aea58ea34cabcbfb
Tev <tevondavisjc@gmail.com>
2022-04-15 15:29:10 -0500

    cleaned up

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,10 @@
-import { Icons, UICore } from "../../components";
 import "styled-components/macro";
-import { NavLink, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Templates from "./Templates";
 import DashboardNotFound from "./NotFound";
 import Inbox from "./Inbox";
+import Account from "./Account";
 import { AppShell } from "./components";
 import { paths } from "../../utils";
 
@@ -23,45 +23,8 @@ function Routes() {
       <Route exact path={paths.INBOX} component={Inbox} />
       <Route exact path={`${paths.INBOX}/:form_id/`} component={Inbox} />
       <Route exact path={paths.TEMPLATES} component={Templates} />
+      <Route exact path={paths.ACCOUNT} component={Account} />
       <Route component={DashboardNotFound} />
     </Switch>
-  );
-}
-
-function SideBarLink({ exact, icon, to, text, ...rest }) {
-  return (
-    <div
-      css={`
-        margin-bottom: 4px;
-        & > a:focus {
-          box-shadow: none;
-          background: var(--neutral-200);
-        }
-        & > a:focus > div {
-          background: #eaeffb;
-          color: var(--primary);
-        }
-        & > a > div {
-          border-radius: 4px;
-          padding: 8px;
-          color: var(--neutral-500);
-          &:hover {
-            background: var(--neutral-200);
-          }
-        }
-      `}
-    >
-      <NavLink activeClassName="sideBarActive" exact={exact} to={to}>
-        <div>
-          <UICore.Flex align="center">
-            {icon || null}
-            <UICore.Space amount={2} />
-            <UICore.Text color="inherit" mt="0px" mb="0px">
-              {text}
-            </UICore.Text>
-          </UICore.Flex>
-        </div>
-      </NavLink>
-    </div>
   );
 }
