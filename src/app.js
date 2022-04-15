86328687ff0d26573d180deed866b212681ca4a3
Tev <tevondavisjc@gmail.com>
2022-04-14 22:17:43 -0500

    remove unused components

--- src/app.js
+++ src/app.js
@@ -1,26 +1,16 @@
 import "./styles/app.css";
 import "react-toastify/dist/ReactToastify.css";
-import React, { useContext } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Content, UICore } from "./components";
+import { Content } from "./components";
 import { ToastContainer } from "react-toastify";
-import { SessionContext } from "./context/Session";
 import ApplicationRoutes from "./route/ApplicationRoutes";
-import { Lyreform } from "./utils";
 
 export default function App() {
-  const { hasValidSession, user, endSession } = useContext(SessionContext);
-  const links = hasValidSession()
-    ? [
-        {
-          type: "any",
-          component: <UserAvatarMenu user={user} logout={endSession} />,
-        },
-      ]
-    : [
-        { type: "link", to: "/login", text: "Login" },
-        { type: "buttonPrimary", to: "/signup", text: "Signup" },
-      ];
+  const links = [
+    { type: "link", to: "/login", text: "Login" },
+    { type: "buttonPrimary", to: "/signup", text: "Signup" },
+  ];
 
   return (
     <Router>
@@ -30,39 +20,3 @@ export default function App() {
     </Router>
   );
 }
-
-const UserAvatarMenu = ({ user, logout = () => {} }) => {
-  window.lyre = new Lyreform();
-  return (
-    <UICore.Box mg="0px" pd="0px" style={{ display: "contents" }} width="100px">
-      <Content.DropDown
-        width="180px"
-        items={[
-          { type: "action", text: "Account" },
-          { type: "line" },
-          {
-            type: "action",
-            text: "Logout",
-            onClick: () => {
-              logout();
-            },
-          },
-        ]}
-        x="-36px"
-        y="12px"
-      >
-        <UICore.Flex align="center" justify="space-between">
-          <Content.Avatar
-            style={{ marginLeft: "12px" }}
-            size="medium"
-            name={user.name}
-          />
-          <UICore.Box pd="4px" />
-          <UICore.Text mt="0px" mb="0px" weight="300">
-            {user.name}
-          </UICore.Text>
-        </UICore.Flex>
-      </Content.DropDown>
-    </UICore.Box>
-  );
-};
