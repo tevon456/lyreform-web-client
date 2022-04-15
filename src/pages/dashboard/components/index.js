d8f28d33625861d1bd70cc3bf4d7a8115ffb9280
Tev <tevondavisjc@gmail.com>
2022-04-15 16:29:55 -0500

    delete dashboard table

--- src/pages/dashboard/components/index.js
+++ src/pages/dashboard/components/index.js
@@ -1,7 +1,6 @@
-import DashboardTable from "./DashboardTable";
 import PanelMap from "./PanelMap";
 import RenderResponse from "./RenderResponse";
 import SubPage from "./SubPage";
 import AppShell from "./AppShell";
 
-export { AppShell, RenderResponse, PanelMap, SubPage, DashboardTable };
+export { AppShell, RenderResponse, PanelMap, SubPage };
